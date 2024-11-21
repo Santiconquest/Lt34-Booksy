29b495071b7c4e3cc0ad8f21306506ec09a91b56
Santiconquest <161180110+Santiconquest@users.noreply.github.com>
2024-11-21 01:33:13 +0000

    Se agrega estilo a pagina login y signup

--- src/front/js/pages/loginCritico.js
+++ src/front/js/pages/loginCritico.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import "../../styles/signup.css";
 
 const LoginCritico = () => {
     const [email, setEmail] = useState('');
@@ -26,30 +27,52 @@ const LoginCritico = () => {
             {store.auth ? (
                 <Navigate to='/listaLibrosCritico' /> 
             ) : (
-                <form className="w-50 mx-auto" onSubmit={sendData}>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                        <input 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            type="email"
-                            className="form-control"
-                            id="exampleInputEmail1"
-                            aria-describedby="emailHelp"
-                        />
+                <section className="container-fluid d-block vh-100 p-0">
+                    <div className="container-fluid d-flex flex-column ms-5">
+                        <div className="row align-items-center justify-content-center gx-0 min-vh-100" >
+                            <div className="col-12 col-md-6 col-lg-4 py-8 py-md-11">
+                                {/* Heading */}
+                                <h1 className="mb-0 fw-bold mt-5">Log in Critic</h1>
+                                {/* Text */}
+                                <p className="mb-6 text-body-secondary">Give us your best feedback</p>
+                                {/* Form */}
+                                <form className="mx-auto" onSubmit={sendData}>
+                                    <div className="mb-3">
+                                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                                        <input 
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            type="email"
+                                            className="form-control"
+                                            id="exampleInputEmail1"
+                                            aria-describedby="emailHelp"
+                                            placeholder="name@address.com"
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                                        <input
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            type="password"
+                                            className="form-control"
+                                            id="exampleInputPassword1"
+                                            placeholder="Enter your password"
+                                        />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary w-100 mt-3">Login</button>
+                                </form>
+                            </div>
+                            <div className="col-lg-7 offset-lg-1 align-self-stretch d-none d-lg-block derecha">
+                                {/* Image */}
+                                <div className="h-100 w-cover bg-cover"></div>
+                                {/* Shape */}
+                                <div className="shape shape-start shape-fluid-y text-white triangle">
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            type="password"
-                            className="form-control"
-                            id="exampleInputPassword1"
-                        />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Login</button>
-                </form>
+                </section>
             )}
         </div>
     );
--- src/front/js/pages/loginLector.js
+++ src/front/js/pages/loginLector.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/login.css";
+import "../../styles/signup.css";
 import { Link, useNavigate } from "react-router-dom";
 
 export const LoginLector = () => {
@@ -9,10 +9,6 @@ export const LoginLector = () => {
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
     const navigate = useNavigate();
-
-    const astronautIcon = "https://via.placeholder.com/150";
-    const emailIcon = "https://via.placeholder.com/50/0000FF/808080?text=Email";
-    const passwordIcon = "https://via.placeholder.com/50/FF0000/FFFFFF?text=Pass";
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -35,65 +31,67 @@ export const LoginLector = () => {
 
     return (
         <>
-            <div className="background"></div>
-            <div className="centering">
-                <form className="my-form" onSubmit={handleSubmit}>
-                    <div className="login-welcome-row">
-                        <img
-                            className="login-welcome"
-                            src={astronautIcon}
-                            alt="Astronaut"
-                        />
-                        <h1>LogIn Lectores!</h1>
-                    </div>
+           <section className="container-fluid d-block vh-100 p-0">
+                <div className="container-fluid d-flex flex-column ms-5">
+                    <div className="row align-items-center justify-content-center gx-0 min-vh-100" >
+                        <div className="col-12 col-md-6 col-lg-4 py-8 py-md-11">
+                            {/* Heading */}
+                            <h1 className="mb-0 fw-bold mt-5">Log in Lectores</h1>
+                            {/* Text */}
+                            <p className="mb-6 text-body-secondary">Became part of a great community</p>
+                            {/* Form */} 
+                            <form className="mx-auto" onSubmit={handleSubmit}>
 
-                    {error && <div className="alert alert-danger" role="alert">{error}</div>}
+                                {error && <div className="alert alert-danger" role="alert">{error}</div>}
 
-                    <div className="text-field">
-                        <label htmlFor="email">Email:</label>
-                        <input
-                            aria-label="Email"
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="Your Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                        <img
-                            alt="Email Icon"
-                            title="Email Icon"
-                            src={emailIcon}
-                        />
-                    </div>
-                    <div className="text-field">
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            id="password"
-                            type="password"
-                            aria-label="Password"
-                            name="password"
-                            placeholder="Your Password"
-                            title="Minimum 6 characters at least 1 Alphabet, 1 Number and 1 Symbol"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                        <img
-                            alt="Password Icon"
-                            title="Password Icon"
-                            src={passwordIcon}
-                        />
-                    </div>
-                    <input type="submit" className="my-form__button" value="Login" />
-                    <div className="my-form__actions">
-                        <div className="my-form__signup">
-                            <Link to="/signupLector" title="Create Account">Eres nuevo? Crea una cuenta</Link>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">Email address</label>
+                                    <input
+                                        aria-label="Email"
+                                        className="form-control"
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        placeholder="name@address.com"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                    <input
+                                        id="password"
+                                        type="password"
+                                        aria-label="Password"
+                                        name="password"
+                                        className="form-control"
+                                        placeholder="Enter your Password"
+                                        title="Minimum 6 characters at least 1 Alphabet, 1 Number and 1 Symbol"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                {/* <input type="submit" className="my-form__button" value="Login" /> */}
+                                <button type="submit" className="btn btn-primary w-100 mb-3 mt-3">Login</button>
+                                <div className="my-form__actions">
+                                    <div className="my-form__signup">
+                                        <Link to="/signupLector" title="Create Account">Eres nuevo? Crea una cuenta</Link>
+                                    </div>
+                                </div>
+                            </form>
+                            </div>
+                            <div className="col-lg-7 offset-lg-1 align-self-stretch d-none d-lg-block derecha">
+                                {/* Image */}
+                                <div className="h-100 w-cover bg-cover"></div>
+                                {/* Shape */}
+                                <div className="shape shape-start shape-fluid-y text-white triangle">
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </form>
-            </div>
+                </section> 
         </>
     );
 };
